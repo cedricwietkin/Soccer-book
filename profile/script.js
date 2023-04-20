61017ff55dbb94403833f3e8b7cbd53e881a8c1f
fle0 <iliassino@live.be>
2023-04-20 18:19:55 +0200

    rg

--- profile/script.js
+++ profile/script.js
@@ -1,36 +1,66 @@
-class carousel {
+class carrousel {
+    
+    /*
+    * @param {HTMLElement} element
+    * @param {objet} options
+    * @param {objet} options.slidesToScroll nombre d'éléments a faire défiler
+    * @param {objet} options.SlideVisible nombre d'élements visible dans un slide
+    */
 
-constructor (element, option = {}) {
-    this.element = element
-    this.option = object.assign({}, {
-        slidesToScroll: 1
-        slidesVisible: 1
-    }, option)
-    this.children = [].slice.call(element.children)
-    let root = this.createDivWithClass('carousel')
-    let container = this.createDivWithClass('carousel-container')
-    root.appendChild(container)
-    this.element.appendChild(root)
-    this.children.forEach(function (child) => {
-        this.createDivWithClass('carousel-item')
-        item.appendChild(child)
-        container.appendChild(item)
-    });
-}
+    constructor (element, options = {}) {
+        this.element = element
+        this.options = Object.assign({}, {
+            slidesToScroll: 1,
+            slidesVisible: 1
+        }, options)
+        let children = [].slice.call(element.children)
+        let root = this.createDivWithClaass('carousel')
+        this.container = this.createDivWithClaass('carousel-container')
+        root.appendChild(this.container)
+        this.element.appendChild(root)
+        this.items = children.map((child) => {
+            let item = this.createDivWithClaass('caroussel-item')
+            item.appendChild(child)
+            this.container.appendChild(item)
+            return item
+        })
+        this.setStyle()
+        this.createNavigation()
+    }
 
-createDivWithClass (className){
+
+
+    /*
+    * applique les bonnes dimensions au carrousel
+    */
+    setStyle () {
+        let ratio = this.items.length / this.options.slidesVisible
+        this.container.style.width = (ratio * 100) + "%"
+        this.item.array.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%")
+    }
+
+
+    createNavigation (){
+
+    }
+
+    /*
+    * @param {string} className
+    * @returns {HTMLElement}
+    */
+
+   createDivWithClaass (className) {
     let div = document.createElement('div')
     div.setAttribute('class', className)
     return div
+   }
 }
 
-}
+document.addEventListener('DOMContentLoaded', function (){
 
-document.addEventListener('DOMContentLoaded', function(){
+    new carrousel (document.querySelector('.carrousel'), {
+        slidesToScroll: 2,
+        slidesVisible: 3
+    })
 
-})
-
-new carousel(document.querySelector('#item1m'), {
-    slidesToScroll: 3
-    slidesVisible: 3
 })
